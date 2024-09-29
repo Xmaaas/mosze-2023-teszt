00b5d93f4f9831d9addca33d3cc62717f9ad465e
Xmaaas <karacsony.gabor.istvan@gmail.com>
2024-09-29 13:59:23 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
+int main()
+{
+    int *b = new int[NELEMENTS];
+    for (int i = 0;)
+    {
+        b[i] = i * 2;
+    }
+    for (int i = 0; i; i++)
+    {
+        std::cout << "Ertek:"
+    }    
+    std::cout << "Atlag szamitasa: " << std::endl;
+    int atlag;
+    for (int i = 0; i < N_ELEMENTS, i++)
+    {
+        atlag += b[i]
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
+    return 0;
+}
